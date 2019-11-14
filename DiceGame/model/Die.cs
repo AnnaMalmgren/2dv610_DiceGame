2f603096d7c0d23850896843198cea710db8b0b2
Anna Malmgren <am223rj@student.lnu.se>
2019-11-14 17:24:24 +0100

    REFACTOR

--- DiceGame/model/Die.cs
+++ DiceGame/model/Die.cs
@@ -6,13 +6,14 @@ namespace DiceGame.model
   public class Die
   {
     private const int minNrOfSides = 4;
+    private const int maxNrOfSides = 12;
     private int _sides;
     public int Sides
     {
       get => _sides;
       private set
       {
-        if (value < minNrOfSides || value > 12)
+        if (value < minNrOfSides || value > maxNrOfSides)
         {
           throw new ArgumentException();
         }
