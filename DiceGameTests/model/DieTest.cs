6e7cd024078084b9bd1fde47f719b876fe46a213
Anna Malmgren <am223rj@student.lnu.se>
2019-11-22 18:01:58 +0100

    REFACTOR

--- DiceGameTests/model/DieTest.cs
+++ DiceGameTests/model/DieTest.cs
@@ -7,18 +7,28 @@ namespace DiceGameTests
 {
   public class DieTest
   {
+      private Die sut;
+
+      private Mock<Random> randMock;
+
+      public DieTest()
+      {
+        this.randMock = new Mock<Random>();
+        this.sut = new Die(this.randMock.Object);
+
+      }
+
       [Fact]
       public void sidesDieDefaultShouldBeSix()
       {
-        Die sut = new Die(new Random());
-        int actual = sut.Sides;
+        int actual = this.sut.Sides;
         Assert.Equal(6, actual);
       }
 
       [Fact]
       public void sidesDieShouldBeTwelve()
       {
-        Die sut = new Die(new Random(), 12);
+        Die sut = new Die(this.randMock.Object, 12);
         int actual = sut.Sides;
         Assert.Equal(12, actual);
       }
@@ -26,23 +36,23 @@ namespace DiceGameTests
       [Fact]
       public void sidesOfDieExceptionIfLessThanFour()
       {
-        Assert.Throws<ArgumentException>(() => new Die(new Random(), 0));
+        Assert.Throws<ArgumentException>(() => new Die(this.randMock.Object, 0));
       }
 
       [Fact]
       public void sidesOfDieExceptionIfMoreThanTwelve()
       {
-        Assert.Throws<ArgumentException>(() => new Die(new Random(), 13));
+        Assert.Throws<ArgumentException>(() => new Die(this.randMock.Object, 13));
       }
 
       [Fact]
       public void sidesOfDieMustBeEvenValue()
       {
-        Assert.Throws<ArgumentException>(() => new Die(new Random(), 5));
+        Assert.Throws<ArgumentException>(() => new Die(this.randMock.Object, 5));
       }
 
       [Fact]
-      public void faceValueShouldReturnValueInRange()
+      public void getFaceValueShouldReturnValueInExpectedRange()
       {
         Die sut = new Die(new Random());
         sut.RollDie();
@@ -52,28 +62,24 @@ namespace DiceGameTests
       }
 
       [Fact]
-      public void faceValueShouldReturnSpecificValue()
+      public void getFaceValueShouldReturnSpecificValueFromRandom()
       {
-        var mockRandom = new Mock<Random>();
-        mockRandom.Setup(m => m.Next(1, 7)).Returns(6);
+        this.randMock.Setup(m => m.Next(1, 7)).Returns(6);
         
-        Die sut = new Die(mockRandom.Object);
-        sut.RollDie();
-        int actual = sut.GetFaceValue();
+        this.sut.RollDie();
+        int actual = this.sut.GetFaceValue();
 
         Assert.Equal(6, actual);
       }
 
       [Fact]
-      public void faceValueShouldBeTheSameWhenDieNotRolled()
+      public void getFaceValueShouldBeTheSameWhenDieNotRolled()
       {
-        var mockRandom = new Mock<Random>();
-        mockRandom.Setup(m => m.Next(1, 7)).Returns(5);
-        Die sut = new Die(mockRandom.Object);
-        sut.RollDie();
+        this.randMock.Setup(m => m.Next(1, 7)).Returns(5);
+        this.sut.RollDie();
 
-        int value1 = sut.GetFaceValue();
-        int value2 = sut.GetFaceValue();
+        int value1 = this.sut.GetFaceValue();
+        int value2 = this.sut.GetFaceValue();
 
         Assert.Equal(value1, value2);
       }
@@ -81,10 +87,8 @@ namespace DiceGameTests
       [Fact]
       public void addSubscribersShouldAddOneSubscriberToList()
       {
-        var mockRandom = new Mock<Random>();
-        Die sut = new Die(mockRandom.Object);
         Mock<IRollDieObserver> subscriber = new Mock<IRollDieObserver>();
-        sut.AddSubscriber(subscriber.Object);
+        this.sut.AddSubscriber(subscriber.Object);
 
         Assert.Equal(1, sut.Subscribers.Count);
       }
